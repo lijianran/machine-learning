511186747ad3cf2ab225e45ed54cd227197fc76c
李简然 <lijianran@outlook.com>
2017-09-10 21:33:42 +0800

    决策树绘制完整代码

--- TREES/tree.py
+++ TREES/tree.py
@@ -3,7 +3,6 @@
 from math import log
 import operator
 
-
 def createDataSet():
     dataSet = [[1, 1, 'yes'],
                [1, 1, 'yes'],
@@ -11,53 +10,49 @@ def createDataSet():
                [0, 1, 'no'],
                [0, 1, 'no']]
     labels = ['no surfacing', 'flippers']
-    # change to discrete values
     return dataSet, labels
 
+def xiangnong(dataSet):
+    size = len(dataSet) #整体数据的数目
+    labelCounts = {} #统计各个标签的次数
+    for i in dataSet:
+        currentLabel = i[-1] #取出每一个特征
+        if currentLabel not in labelCounts.keys(): #特征第一次统计先初始化为0
+            labelCounts[currentLabel] = 0
+        labelCounts[currentLabel] += 1 #然后统计出现次数
+    xiangnong = 0.0
+    for key in labelCounts: #循环总和所有特征的香农熵
+        prob = float(labelCounts[key]) / size #计算每个特征出现的频率
+        xiangnong -= prob * log(prob, 2) #计算香农熵
+    return xiangnong
 
-def calcShannonEnt(dataSet):
-    numEntries = len(dataSet)
-    labelCounts = {}
-    for featVec in dataSet:  # the the number of unique elements and their occurance
-        currentLabel = featVec[-1]
-        if currentLabel not in labelCounts.keys(): labelCounts[currentLabel] = 0
-        labelCounts[currentLabel] += 1
-    shannonEnt = 0.0
-    for key in labelCounts:
-        prob = float(labelCounts[key]) / numEntries
-        shannonEnt -= prob * log(prob, 2)  # log base 2
-    return shannonEnt
-
-
-def splitDataSet(dataSet, axis, value):
+def splitDataSet(dataSet, axis, value): #分离选中特征
     retDataSet = []
     for featVec in dataSet:
         if featVec[axis] == value:
-            reducedFeatVec = featVec[:axis]  # chop out axis used for splitting
-            reducedFeatVec.extend(featVec[axis + 1:])
-            retDataSet.append(reducedFeatVec)
+            reducedFeatVec = featVec[:axis] #取出0到axis的数据
+            reducedFeatVec.extend(featVec[axis + 1:]) #在后面追加axis+1到最后的数据
+            retDataSet.append(reducedFeatVec) #返回分离了第axis列数据的数据集
     return retDataSet
 
-
-def chooseBestFeatureToSplit(dataSet):
-    numFeatures = len(dataSet[0]) - 1  # the last column is used for the labels
-    baseEntropy = calcShannonEnt(dataSet)
-    bestInfoGain = 0.0;
+def chooseBestFeatureToSplit(dataSet): #选取最好的用于分类的特征
+    numFeatures = len(dataSet[0]) - 1 #可以用于分类的特征的总数目
+    baseXiangnong = xiangnong(dataSet) #先计算数据集基础原始的香农熵
+    k = 0.0
     bestFeature = -1
-    for i in range(numFeatures):  # iterate over all the features
-        featList = [example[i] for example in dataSet]  # create a list of all the examples of this feature
-        uniqueVals = set(featList)  # get a set of unique values
-        newEntropy = 0.0
-        for value in uniqueVals:
-            subDataSet = splitDataSet(dataSet, i, value)
+    for i in range(numFeatures): #计算每一个特征用于分类之后的香农熵的变化，选取当前数据集最佳的分离特征
+        featureList = [example[i] for example in dataSet] #取出每一个特征的可能取值
+        uniqueFeature = set(featureList)  #唯一化取值
+        newXiangnong = 0.0
+        for value in uniqueFeature: #计算每一个特征值用来分类后的香农熵
+            subDataSet = splitDataSet(dataSet, i, value) #分离出某特征的某一个取值后的数据集
             prob = len(subDataSet) / float(len(dataSet))
-            newEntropy += prob * calcShannonEnt(subDataSet)
-        infoGain = baseEntropy - newEntropy  # calculate the info gain; ie reduction in entropy
-        if (infoGain > bestInfoGain):  # compare this to the best gain so far
-            bestInfoGain = infoGain  # if better than current best, set to best
+            newXiangnong += prob * xiangnong(subDataSet)
+        change = baseXiangnong - newXiangnong #计算变化值
+        if (change > k):  #每次循环都更新为信息复杂度更低的即香农熵更小的特征
+            k = change
             bestFeature = i
-    return bestFeature  # returns an integer
-
+    return bestFeature
 
 def majorityCnt(classList):
     classCount = {}
@@ -67,21 +62,38 @@ def majorityCnt(classList):
     sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
 
-
 def createTree(dataSet, labels):
-    classList = [example[-1] for example in dataSet]
-    if classList.count(classList[0]) == len(classList):
-        return classList[0]  # stop splitting when all of the classes are equal
-    if len(dataSet[0]) == 1:  # stop splitting when there are no more features in dataSet
-        return majorityCnt(classList)
-    bestFeat = chooseBestFeatureToSplit(dataSet)
-    bestFeatLabel = labels[bestFeat]
+    classList = [example[-1] for example in dataSet] #取出当前数据集的所有特征的取值
+    if classList.count(classList[0]) == len(classList): #如果第一个特征的这一取值总数已经等于统计的总数
+        return classList[0]  #说明已经分类完成
+    if len(dataSet[0]) == 1:  #如果数据集只剩下一列，说明使用完所有特征都不能把原有数据集分离出仅包含唯一类别的分组
+        return majorityCnt(classList) #挑选出出现次数最多的类别作为返回值
+    bestFeature = chooseBestFeatureToSplit(dataSet) #选出当前数据集最佳用于分类的特征
+    bestFeatLabel = labels[bestFeature]
     myTree = {bestFeatLabel: {}}
-    del (labels[bestFeat])
-    featValues = [example[bestFeat] for example in dataSet]
+    del (labels[bestFeature])
+    featValues = [example[bestFeature] for example in dataSet]
     uniqueVals = set(featValues)
     for value in uniqueVals:
-        subLabels = labels[:]  # copy all of labels, so trees don't mess up existing labels
-        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value), subLabels)
+        subLabels = labels[:] #复制原有标签，以免更改了原有标签
+        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeature, value), subLabels)
     return myTree
+
+def classify(inputTree, featureLabels, test):
+    firstLabel = inputTree.keys()[0] #从第一个键开始
+    secondDirectory = inputTree[firstLabel] #取出字典
+    featureIndex = featureLabels.index(firstLabel) #index查找当前列表中第一个匹配Label的元素
+    # key = test[featureIndex]
+    # value = secondDirectory[key]
+    # if isinstance(value,dict): #isinstance是Python中的一个内建函数，用来判断一个对象是否是一个已知的类型
+    #     classLabel = classify(value, featureLabels, test)
+    # else:
+    #     classLabel = value
+    for key in secondDirectory.keys():
+        if test[featureIndex] == key:
+            if type(secondDirectory[key]).__name__ == 'dict':
+                classLabel = classify(secondDirectory[key], featureLabels, test)
+            else:
+                classLabel = secondDirectory[key]
+    return classLabel
 
--- TREES/treePlotter.py
+++ TREES/treePlotter.py
@@ -14,10 +14,68 @@ def plotNode(nodeTxt, centerPt, parentPt, nodeType):
              xytext=centerPt, textcoords='axes fraction',
              va="center", ha="center", bbox=nodeType, arrowprops=arrow_args )
 
-def createPlot():
+# def createPlot():
+#     fig = plt.figure(1, facecolor='white')
+#     fig.clf()
+#     createPlot.ax1 = plt.subplot(111, frameon=False)
+#     plotNode('决策节点',(0.5,0.1),(0.1,0.5),decisionNode)
+#     plotNode('叶节点',(0.8,0.1),(0.3,0.8),leafNode)
+#     plt.show()
+
+def getLeafNum(myTree): #获取节点数目，以确定x轴的长度
+    LeafNum = 0
+    firstLabel = myTree.keys()[0] #从第一个键开始
+    secondDirection = myTree[firstLabel] #取出字典
+    for key in secondDirection.keys(): #循环取出字典中的键
+        if type(secondDirection[key]).__name__=='dict': #如果取出的键对应的值的类型为字典
+            LeafNum += getLeafNum(secondDirection[key]) #则递归继续统计节点
+        else:
+            LeafNum += 1 #不是字典则表明到某一分支末节点
+    return LeafNum
+
+def getTreeDepth(myTree):
+    TreeDepth = 0
+    firstLabel = myTree.keys()[0] #从第一个键开始
+    secondDirection = myTree[firstLabel] #取出字典
+    for key in secondDirection.keys(): #循环取出字典中的键
+        if type(secondDirection[key]).__name__ == 'dict':  # 如果取出的键对应的值的类型为字典
+            thisTreeDepth = 1 + getTreeDepth(secondDirection[key]) #不断递归到最深节点处
+        else:
+            thisTreeDepth = 1 #不是字典则表明到某一分支末节点,更新此处深度为1
+        if thisTreeDepth > TreeDepth: #获取最深的节点深度
+            TreeDepth = thisTreeDepth
+    return TreeDepth
+
+def plotMidText(cntrPt, parentPt, txtString): #在父子节点之间填充文本信息
+    xMid = (parentPt[0]-cntrPt[0])/2.0 + cntrPt[0]
+    yMid = (parentPt[1]-cntrPt[1])/2.0 + cntrPt[1]
+    createPlot.ax1.text(xMid, yMid, txtString, va="center", ha="center", rotation=30)
+
+def plotTree(myTree, parentPt, nodeTxt):
+    width = getLeafNum(myTree)
+    depth = getTreeDepth(myTree)
+    firstLabel = myTree.keys()[0]
+    cntrPt = (plotTree.xOff + (1.0 + float(width))/2.0/plotTree.totalW, plotTree.yOff)
+    plotMidText(cntrPt, parentPt, nodeTxt)
+    plotNode(firstLabel, cntrPt, parentPt, decisionNode)
+    secondDict = myTree[firstLabel]
+    plotTree.yOff = plotTree.yOff - 1.0/plotTree.totalD
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__=='dict':
+            plotTree(secondDict[key],cntrPt,str(key))
+        else:
+            plotTree.xOff = plotTree.xOff + 1.0/plotTree.totalW
+            plotNode(secondDict[key], (plotTree.xOff, plotTree.yOff), cntrPt, leafNode)
+            plotMidText((plotTree.xOff, plotTree.yOff), cntrPt, str(key))
+    plotTree.yOff = plotTree.yOff + 1.0/plotTree.totalD
+
+def createPlot(inTree):
     fig = plt.figure(1, facecolor='white')
     fig.clf()
-    createPlot.ax1 = plt.subplot(111, frameon=False)
-    plotNode('决策节点',(0.5,0.1),(0.1,0.5),decisionNode)
-    plotNode('叶节点',(0.8,0.1),(0.3,0.8),leafNode)
+    axprops = dict(xticks=[], yticks=[])
+    createPlot.ax1 = plt.subplot(111, frameon=False, **axprops)
+    plotTree.totalW = float(getLeafNum(inTree))
+    plotTree.totalD = float(getTreeDepth(inTree))
+    plotTree.xOff = -0.5/plotTree.totalW; plotTree.yOff = 1.0
+    plotTree(inTree, (0.5,1.0), '')
     plt.show()
